384bbf9c76c07f5443181bc810f7f966d57c1f04
RowdyOnline <rowdy14@gmail.com>
2020-11-25 10:11:45 -0800

    First draft

---
+++ openWeather/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(`#city-form`).submit(function(e){
+        e.preventDefault()
+        let city_input = $(`#city`).val()
+        let url = `api.openweathermap.org/data/2.5/weather?q=${city_input}&appid=${config.myKey}`
+        $.get(url, function(data){
+            $('.output').text(`${data.main.temp}`);
+        }, `json`);
+        return false;
+    })
+})
