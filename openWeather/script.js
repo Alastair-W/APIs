62c2aa2c66d21c88193abf48dc23faa4caec3cf9
RowdyOnline <rowdy14@gmail.com>
2020-11-25 10:18:52 -0800

    Update

--- openWeather/script.js
+++ openWeather/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $(`#city-form`).submit(function(e){
         e.preventDefault()
-        let city_input = $(`#city`).val()
+        let city_input = $(`#city`).val();
         let url = `api.openweathermap.org/data/2.5/weather?q=${city_input}&appid=${config.myKey}`
         $.get(url, function(data){
             $('.output').text(`${data.main.temp}`);
